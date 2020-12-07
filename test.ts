04fe928c9990595200f6c700258c25eda968ff1c
Nitsan Avni <nitsan@papayaglobal.com>
2020-12-07 20:17:57 +0200

    2nd attempt w generics

--- test.ts
+++ test.ts
@@ -1,18 +1,32 @@
-import { Assert, AssertTrue, IsExact } from "conditional-type-checks";
+import { AssertTrue, IsExact } from "conditional-type-checks";
 
 type Table = "t1" | "t2" | "t3";
 type property = "table";
 
+// not used
 interface HasTable {
     table: Table;
 }
 
-const hasTable1 = { table: "t1" as const };
-const hasTable2 = { table: "t2" as const };
-const hasTable3 = { table: "t3" as const };
+interface HasTable2<T extends Table> {
+    table: T;
+}
+
+const hasTable1 = { table: "t1" as "t1" };
+const hasTable2 = { table: "t2" as "t2" };
+const hasTable3 = { table: "t3" as "t3" };
+
+const hasTable21: HasTable2<"t1"> = { table: "t1" };
+const hasTable22: HasTable2<"t2"> = { table: "t2" };
+const hasTable23: HasTable2<"t3"> = { table: "t3" };
 
 const partialCollection = [hasTable1, hasTable2];
 const fullCollection = [hasTable1, hasTable2, hasTable3];
 
+const partialCollection2 = [hasTable21, hasTable22];
+const fullCollection2 = [hasTable21, hasTable22, hasTable23];
+
 type test1 = AssertTrue<IsExact<typeof partialCollection[number][property], Table>>;
 type test2 = AssertTrue<IsExact<typeof fullCollection[number][property], Table>>;
+type test21 = AssertTrue<IsExact<typeof partialCollection2[number][property], Table>>;
+type test22 = AssertTrue<IsExact<typeof fullCollection2[number][property], Table>>;
