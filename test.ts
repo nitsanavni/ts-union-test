5cc8b8a3547b72f8ffc4a9c064b0e761a6124023
Nitsan Avni <nitsan@papayaglobal.com>
2020-12-08 09:58:41 +0200

    with actual tests

--- test.ts
+++ test.ts
@@ -1,32 +1,53 @@
-import { AssertTrue, IsExact } from "conditional-type-checks";
+import test from "ava";
+import { command } from "execa";
+import * as fs from "fs";
+import { promisify } from "util";
 
-type Table = "t1" | "t2" | "t3";
-type property = "table";
+const writeFile = promisify(fs.writeFile);
+const unlink = promisify(fs.unlink);
 
-// not used
-interface HasTable {
-    table: Table;
-}
+const buildTsc = async (tsCode: string) => {
+    const path = `./${Date.now()}`;
+    await writeFile(`${path}.ts`, Buffer.from(tsCode));
+    const { all } = await command(`npx tsc ${path}.ts`, { all: true, reject: false });
 
-interface HasTable2<T extends Table> {
-    table: T;
-}
+    await unlink(`${path}.ts`);
+    await unlink(`${path}.js`);
 
-const hasTable1 = { table: "t1" as "t1" };
-const hasTable2 = { table: "t2" as "t2" };
-const hasTable3 = { table: "t3" as "t3" };
+    return `${all}`;
+};
 
-const hasTable21: HasTable2<"t1"> = { table: "t1" };
-const hasTable22: HasTable2<"t2"> = { table: "t2" };
-const hasTable23: HasTable2<"t3"> = { table: "t3" };
+test.serial("scaffolding - generate and build a ts file", async (t) => {
+    const tsCodeWithBuildError = `
+        type A = "a";
+        const b: A = "b";
+    `;
 
-const partialCollection = [hasTable1, hasTable2];
-const fullCollection = [hasTable1, hasTable2, hasTable3];
+    const output = await buildTsc(tsCodeWithBuildError);
 
-const partialCollection2 = [hasTable21, hasTable22];
-const fullCollection2 = [hasTable21, hasTable22, hasTable23];
+    t.regex(output, /error TS2322/);
+    t.regex(output, /is not assignable/);
+});
 
-type test1 = AssertTrue<IsExact<typeof partialCollection[number][property], Table>>;
-type test2 = AssertTrue<IsExact<typeof fullCollection[number][property], Table>>;
-type test21 = AssertTrue<IsExact<typeof partialCollection2[number][property], Table>>;
-type test22 = AssertTrue<IsExact<typeof fullCollection2[number][property], Table>>;
+test.serial("our case", async (t) => {
+    const givenTs = `
+    type Table = "t1" | "t2" | "t3";
+
+    const hasTable1 = { table: "t1" as "t1" };
+    const hasTable2 = { table: "t2" as "t2" };
+    const hasTable3 = { table: "t3" as "t3" };
+
+    const partialCollection = [hasTable1, hasTable2];
+    const fullCollection = [hasTable1, hasTable2, hasTable3];
+    `;
+
+    const assertion = (collection: "partialCollection" | "fullCollection") =>
+        `
+        import { AssertTrue, IsExact } from "conditional-type-checks";
+        type property = "table";
+        type test1 = AssertTrue<IsExact<typeof ${collection}[number][property], Table>>;
+        `;
+
+    t.regex(await buildTsc(givenTs + assertion("partialCollection")), /error/);
+    t.notRegex(await buildTsc(givenTs + assertion("fullCollection")), /error/);
+});
